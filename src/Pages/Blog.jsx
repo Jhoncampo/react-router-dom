11bc33c23a4b52e7003f7ecf1d4407d9ea72bf96
Jhon <eduardcamayo13@gmail.com>
2023-11-15 16:38:26 -0500

    SPA filtros

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,14 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useFetch } from "../Hooks/useFetch";
 import { Link, useSearchParams } from "react-router-dom";
 
 const Blog = () => {
-
-    const [searchParams, setSearchParams ] = useSearchParams()
-
-    useEffect(()=>{
-        console.log(searchParams.get("name"))
-    },[searchParams])
+    const [searchParams, setSearchParams] = useSearchParams();
 
     const { data, loading, error } = useFetch(
         "https://jsonplaceholder.typicode.com/posts/"
@@ -16,20 +11,44 @@ const Blog = () => {
     if (loading) return <p>Loading data...</p>;
     if (error) return <p>{error}</p>;
 
+    const handleChange = (e) => {
+        let filter = e.target.value;
+        if (filter) {
+            setSearchParams({ filter });
+        } else {
+            setSearchParams({});
+        }
+    };
+
     return (
         <>
             <h1>Blog</h1>
+            <input
+                type="text"
+                name="filter"
+                value={searchParams.get("filter") || ""}
+                onChange={handleChange}
+                placeholder="Buscar..."
+                className="form-control my-3"
+            />
             <ul className="list-group">
-                {data.map((item) => (
-                    <Link
-                        to={`/blog/${item.id}`}
-                        key={item.id}
-                        className="list-group-item"
-                    >
-                        {`${item.id} - `}
-                        {item.title}
-                    </Link>
-                ))}
+                {data
+                    .filter((item) => {
+                        let filter = searchParams.get("filter");
+                        if (!filter) return true;
+                        let name = item.title.toLocaleLowerCase();
+                        return name.startsWith(filter.toLocaleLowerCase());
+                    })
+                    .map((item) => (
+                        <Link
+                            to={`/blog/${item.id}`}
+                            key={item.id}
+                            className="list-group-item"
+                        >
+                            {`${item.id} - `}
+                            {item.title}
+                        </Link>
+                    ))}
             </ul>
         </>
     );
